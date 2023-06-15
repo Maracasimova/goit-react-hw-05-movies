1cb8667c492a3e647b70a703ee5eee22b0056133
Casimova Marina <maracasimova@gmail.com>
2023-06-15 20:35:18 +0300

    Pages Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { getTrendingMovies } from '../../API/get-trending-movies';
+import { useLocation } from 'react-router-dom';
+import styles from './Home.module.css';
+import MovieList from 'components/MoviesList/MoviesList.jsx';
+
+export default function Home() {
+  const location = useLocation();
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const { results } = await getTrendingMovies();
+
+      setMovies(results);
+    };
+
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1 className={styles.title}>Trending today</h1>
+      <MovieList movies={movies} location={location} />
+    </div>
+  );
+}
